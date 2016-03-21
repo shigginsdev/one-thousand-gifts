a61e21d0afe6db757b9d019d31801b004e7eee36
Stephanie Higgins <shigginsdev@gmail.com>
2016-03-21 20:03:01 +0000

    added dynamic titles

--- test/controllers/static_pages_controller_test.rb
+++ test/controllers/static_pages_controller_test.rb
@@ -4,16 +4,19 @@ class StaticPagesControllerTest < ActionController::TestCase
   test "should get home" do
     get :home
     assert_response :success
+    assert_select "title", "Home | One Thousand Gifts"
   end
 
   test "should get help" do
     get :help
     assert_response :success
+    assert_select "title", "Help | One Thousand Gifts"
   end
 
   test "should get about" do
     get :about
     assert_response :success
+    assert_select "title", "About | One Thousand Gifts"
   end
 
 end
